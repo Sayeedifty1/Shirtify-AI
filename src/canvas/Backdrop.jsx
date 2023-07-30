26137f0adfe03e03b51830fea569b84527b556ee
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-07-30 22:05:08 +0600

    shirt shadow added

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,7 +1,36 @@
+import { easing } from "maath"
+import { useFrame } from "@react-three/fiber"
+import { AccumulativeShadows , RandomizedLight} from "@react-three/drei"
+import { useRef } from "react"
+
 
 const Backdrop = () => {
+  const shadows = useRef();
   return (
-    <div>Backdrop</div>
+    <AccumulativeShadows
+    ref={shadows}
+    temporal
+    frames={60}
+    alphaTest={0.85}
+    scae={10}
+    rotation={[Math.PI / 2, 0, 0]}
+    position={[0,0,-0.14]}
+    >
+      <RandomizedLight 
+      amount={4}
+      radius={9}
+      intensity={2.5}
+      ambient={0.25}
+      position={[5,5,-10]}
+      />
+      <RandomizedLight 
+      amount={5}
+      radius={7}
+      intensity={1}
+      ambient={0.55}
+      position={[-5,7,-9]}
+      />
+    </AccumulativeShadows>
   )
 }
 
