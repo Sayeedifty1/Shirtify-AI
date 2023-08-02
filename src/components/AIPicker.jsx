332daf0e231ca75df2f685b8910c9e094db84d19
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-08-03 01:53:31 +0600

    complete

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,7 +1,29 @@
+import CustomButton from "./CustomButton"
 
-const AIPicker = () => {
+
+
+const AIPicker = ({prompt , setPrompt , generatingImg , handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className="aipicker-container">
+      <textarea 
+      placeholder="Ask AI to generate an image for you..."
+      rows={5}
+      value={prompt}
+      onChange={(e) => setPrompt(e.target.value)}
+      className="aipicker-textarea" />
+      <div className="flex flex-wrap gap-3">
+      {generatingImg? (
+        <CustomButton type="outline" title="Generating..." customStyles="text-xs" />
+      ): (
+        <>
+        <CustomButton type="outline" title="Generate logo" customStyles="text-xs" handleClick={()=>handleSubmit('logo')} />
+       
+        <CustomButton type="filled" title="Generate full" customStyles="text-xs" handleClick={()=>handleSubmit('logo')} />
+        </>
+      )}
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -39,12 +39,42 @@ const Customizer = () => {
           readFile={readFile}
         />
       case "aipicker":
-        return <AIPicker />
+        return <AIPicker
+        prompt={prompt}
+        setPrompt={setPrompt}
+        generatingImg={generatingImg}
+        handleSubmit={handleSubmit}
+        />
       default:
         return null;
     }
   }
 
+  const handleSubmit = async (type) => {
+  if(!prompt) return alert("Please enter a prompt");
+
+  try {
+      setGeneratingImg(true);
+      const res = await fetch("http://localhost:8080/api/v1/dalle",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+      const data = await res.json();
+
+      handleDecals( type, `data:image/png;base64,${data.photo}`);
+  } catch (error) {
+    alert(error)
+  } finally{
+    setGeneratingImg(false);
+    setActiveEditorTab("");
+  }
+
+  }
   const handleDecals = (result, type) => {
     const decalType = DecalTypes[type];
 
@@ -61,6 +91,7 @@ const Customizer = () => {
         break;
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName];
+        break;
       default:
         state.isLogoTexture = true;
         state.isFullTexture = false;
